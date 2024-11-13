8fe313951ede226a897b140873019c63a20799a5
Christian-Skovgaard <cellochr@gmail.com>
2024-11-13 17:04:05 +0100

    mongodb2

--- API.js
+++ API.js
@@ -6,36 +6,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const arr = [{
-    'navn':'træningscykel',
-    'muskelgrupper': [
-        'ben'
-    ]},
-    {'navn':'let_puldown',
-    'muskelgrupper':['ryg','biceps']},
-    {'navn':'armcurl',
-        'muskelgrupper':['biceps']}
-    ]
-
-
 app.get('/hello', (req, res) => {
-    res.send(const MongoClient = require("mongodb");
-    const client = new MongoClient(uri);
-
-    const database = client.db('fitness');
-    const machines = database.collection('machines');
-
-// Query for a movie that has the title 'Back to the Future'
-    const query = { navn: 'Kaj' };
-    const machine = machines.findOne(query);
-
-    console.log(machine);
-)
+    res.send()
 })
 
 
 
-const { MongoClient } = require("mongodb");
 const client = new MongoClient(uri);
 
 const database = client.db('fitness');
