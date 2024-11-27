c1a6e4731dcbb591ccf57a40e3c33b70c71c81a2
Christian-Skovgaard <cellochr@gmail.com>
2024-11-27 12:29:31 +0100

    frontend is now talking with backend!

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-//funtionality for seaching
-const getActiveCheckboxArr = () => {
+// -- funtionality for searching --
+const getActiveCheckboxArr = (filterId) => {
     //function that loops that itterates through all checkboxes and determens withch are checked, then returns as arr
     //denne funktion virker uanset hvor mange filters vi tilføjer fordi den bare tager contetet af vores label for checkboxen, så vi kan bare føje flere til
     const activeCheckboxArr = []
-    const inputDomArr = document.querySelectorAll('header>nav>section>div>input')
+    const inputDomArr = document.querySelectorAll(`#${filterId}>div>input`)
     //vi går igennem alle vores filtre og checker om de er checked eller ej
     for (let i = 0; i < inputDomArr.length; i++) {
-        if (inputDomArr[i].checked !== true) {
+        if (inputDomArr[i].checked === true) {
             //her finder vi contenten af det label som er tilknyttet checkboxen of tilføjer den content til vores array
-            const checkboxLabel = document.querySelector(`header>nav>section>div:nth-child(${i + 1})>label`)
+            const checkboxLabel = document.querySelector(`#${filterId}>div:nth-child(${i + 1})>label`)
             activeCheckboxArr.push(checkboxLabel.textContent)
         }
     }
@@ -28,11 +28,20 @@ const getAPIStringFromArr = (arr) => {
     return string
 }
 
-function searchDBForMachines (name,muscleArr) {
-    fetch(`http://localhost:3000/search/${name}`)
+console.log(getActiveCheckboxArr('difficultyFilter'))
+
+function searchDBForMachines () {
+    const name = getSearchbarContent()
+    const mucleArr= getActiveCheckboxArr('mucleFilter')
+    const formattetMucleArr = getAPIStringFromArr(mucleArr)
+    const difficultyArr= getActiveCheckboxArr('difficultyFilter')
+    const formattetdifficultyArr = getAPIStringFromArr(mucleArr)
+    fetch(`http://localhost:3000/search/name=${name};musclegroup=${formattetMucleArr};difficulty=${formattetdifficultyArr}`)
         .then(response => response.json())
-        .then(data => {return data})
+        .then(jsondata => {console.log(jsondata)})
 }
+
+document.querySelector('#searchButton').addEventListener('click',searchDBForMachines)
 
 function logThis () {
     console.log(getSearchbarContent())
