380c03cc60e20942ff680e970bf8e0ffa2720234
Christian-Skovgaard <cellochr@gmail.com>
2024-12-01 20:08:56 +0100

    made the map update when you search

--- script.js
+++ script.js
@@ -50,8 +50,8 @@ function updateInfoTap(machineArr) {
     const infoSectionDom = document.querySelector('#infoTab')
     let htmlString = ""     //this is the string we are inserting into the html to create the different divs with info
     for (machine of machineArr) {
-        console.log(machine.musclegroup)
-        console.log(getHTMLListFromArr(machine.musclegroup))
+        //console.log(machine.musclegroup)
+        //console.log(getHTMLListFromArr(machine.musclegroup))
         const divString =
             `<div class="infobox">
                    <h3>${machine.name}</h3>
@@ -62,10 +62,35 @@ function updateInfoTap(machineArr) {
              </div>`
         htmlString = htmlString + divString
     }
-    console.log(htmlString)
+    //console.log(htmlString)
     infoSectionDom.innerHTML = htmlString   //finally, we insert the htmlstring into the document
 }
 
+
+// -- map functionality --
+const selectedMachineNamesArr = []  //here we will have the names of all active machines that is selected on the map
+
+const updateSelectedMachineNamesArrWithJasonData = (searchjson) => {    //this function-name might be a bit to long
+    //this just takes the jason and puts it in the selectedMachineNamesArr, because it looks nicer to have it clear in the searchDB function
+    for (obj of searchjson) {
+        selectedMachineNamesArr.push(obj.name)
+    }
+}
+
+function updateSelectedMachines () {    //with this function we make the unselected machine class useless, the css properties of the selectedMachine-class will be on top since it is apllied after the "normal" styling
+    //we reset the map by removing all selectedMachine-classes
+    const allMachinesDOMArr = document.querySelectorAll('#map>div')
+    for (machineDOM of allMachinesDOMArr) {
+        machineDOM.classList.remove('selectedMachine')
+    }
+    //we add the selected machine back to all the machines in our array
+    for (machineName of selectedMachineNamesArr) {
+        const machineDOMArr = document.querySelectorAll(`#map>.${machineName}`)
+        for (machineDOM of machineDOMArr) {
+            machineDOM.classList.add('selectedMachine')
+        }
+    }
+}
 
 // -- funtionality for searching --
 const getActiveCheckboxArr = (filterId) => {
@@ -108,7 +133,8 @@ function searchDBForMachines() {
         .then(jsondata => {
             console.log(jsondata)
             updateInfoTap(jsondata)
-
+            updateSelectedMachineNamesArrWithJasonData(jsondata)
+            updateSelectedMachines()
         })
 }
 
@@ -118,7 +144,7 @@ function logThis() {
     console.log(getSearchbarContent())
 }
 
-//login functionality
+
 
 /*
 //Checkbox for machines functionality
